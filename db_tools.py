b42693a56ca1d24d71d8f9bda4b834ae8448cfdf
Tom Kooij <tomkooij@tomkooij.nl>
2016-12-08 15:06:49 +0100

    Clean-up db_tools.py

--- db_tools.py
+++ db_tools.py
@@ -1,76 +1,33 @@
 # several function used for development of aanmeldr
-#   will be replaced by a toolchain for setting up the database
+#   should be replaced by a toolchain for setting up the database
 #
-#
-# usage: set up (test) database:
-#  from db_tools import *
-#  write_testdb()
-#  write_workshops()
-#  print_db()
-
-
-
-#import sqlite3
 import MySQLdb
 import MySQLdb.cursors
 
 import sys
-import hashlib, os # crypto voor wachtwoorden
+import hashlib, os  # crypto voor wachtwoorden
 import random
 import csv
 import random
 
-# configuration (DATABASE location)
-#from configuration import DATABASE
+# configuration.py bevat MYSQLSERVER, MYSQLPASS e.d.
 from configuration import *
 
-PASSWORD = MYSQLPASS
+# ToDo: Haal dit uit user table:
 AANTAL_LLN = 602
 
-def print_db():
-    db = MySQLdb.connect(host=MYSQLSERVER, user=MYSQLUSER, db=MYSQLDB, passwd=MYSQLPASS)
 
-    with db:
-#         db.row_factory = sqlite3.Row   # this enables the dictionary cursor
-
-        cur = db.cursor()
-        cur.execute("SELECT * FROM users")
-
-        rows = cur.fetchall()
-
-        for i, row in enumerate(rows):
-            print i, row
-
-#        for row in rows:
-#            print "%s %s" % (row["naam"], row["keuze"]) # use the dictionary cursor
-
-
-def print_workshops():
-  #db = sqlite3.connect(DATABASE)
-  db = MySQLdb.connect(host=MYSQLSERVER, user=MYSQLUSER, db=MYSQLDB, passwd=MYSQLPASS)
-
-  with db:
-#      db.row_factory = MySQLdb.Row   # this enables the dictionary cursor
-
-      cur = db.cursor()
-      cur.execute("SELECT * FROM workshops")
-
-      rows = cur.fetchall()
-
-      for row in rows:
-          print row
-
-klas1 = 2**1 # doet niet mee!!!
+klas1 = 2**1  # doet niet mee!!!
 klas2 = 2**2
 klas3 = 2**3
 klas4 = 2**4
 klas5 = 2**5
 klas6 = 2**6
+
 # LET OP!!! KLAS 1 doet niet mee!!
 onderbouw = klas2+klas3
 bovenbouw = klas4+klas5+klas6
 alles = onderbouw+bovenbouw
-
 
 
 workshops_invoer = [ ("Geen keuze ", 100000, alles),
@@ -84,7 +41,7 @@ workshops_invoer = [ ("Geen keuze ", 100000, alles),
     ("Vogels kijken ", 5, klas2),
     ("Schaken ", 5, klas2),
     ("Yoga/mediteren ", 2, klas2),
-#klas 3
+# klas 3
     ("Sport in Mammoet ", 40, klas3),
     ("Circus in Mammoet ", 10, klas3),
     ("Workshop Fotografie ", 4, klas3),
@@ -95,7 +52,7 @@ workshops_invoer = [ ("Geen keuze ", 100000, alles),
     ("Workshop Messiah (Om) ", 5, klas3),
     ("Schaken", 4, klas3),
     ("Yoga/mediteren ", 8, klas3),
-#klas 4
+# klas 4
     ("Step by Step ", 40, klas4),
     ("Waterpolo ", 20, klas4),
     ("Bootcamp met Noor ", 20, klas4),
@@ -105,7 +62,7 @@ workshops_invoer = [ ("Geen keuze ", 100000, alles),
     ("Vogels kijken", 5, klas4),
     ("Schaken", 4, klas4),
     ("Yoga/mediteren ", 3, klas4),
-#klas 5+6
+# klas 5+6
     ("Film Premiere: I, Daniel Blake. Cinema Gouda", 195, klas5+klas6),
     ("Workshop kunstpunt: 3D tekenen", 12, klas5+klas6),
     ("Workshop kunstpunt: Animatiefilmpje", 15, klas5+klas6),
@@ -120,23 +77,47 @@ for idx, workshop in enumerate(workshops_invoer):
     workshops.append(wkshop)
 
 
-def write_workshops():
+def get_db():
+    return MySQLdb.connect(host=MYSQLSERVER, user=MYSQLUSER,
+                           db=MYSQLDB, passwd=MYSQLPASS)
 
+
+def print_db():
+    db = get_db()
+
+    with db:
+        cur = db.cursor()
+        cur.execute("SELECT * FROM users")
+
+        rows = cur.fetchall()
+
+        for i, row in enumerate(rows):
+            print i, row
+
+
+def print_workshops():
+    db = get_db()
+
+    with db:
+    cur = db.cursor()
+    cur.execute("SELECT * FROM workshops")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print row
+
+
+def write_workshops():
 
     # deze functie OVERSCHRIJFT DE DATABASE INCLUSIEF KEUZES!
     print "Let's don't and say we did!"
     return 1
 
-    db = MySQLdb.connect(host=MYSQLSERVER, user=MYSQLUSER, db=MYSQLDB, passwd=MYSQLPASS)
+    db = get_db()
 
     with db:
-
         cur = db.cursor()
-        #      cur.execute("INSERT INTO users VALUES (3145,'Pi','wachtwoord',1);")
-        #      cur.execute("INSERT INTO users VALUES (2789,'EEeee','wachtwoord',3);")
-        #      cur.execute("INSERT INTO users VALUES (007,'James Bond','wachtwoord',7);")
-        #      cur.execute("INSERT INTO users VALUES (1,'Test Leerling','geheim',1);")
-
 
         cur.execute("DROP TABLE IF EXISTS workshops")
         cur.execute("CREATE TABLE workshops(id INT, titel TEXT, plaatsen INT, filter INT )")
@@ -148,32 +129,6 @@ def write_workshops():
         for workshop in workshops:
             aantal_plaatsen += workshop[2]
         print "controleer: %d plaatsen" % aantal_plaatsen
-
-
-def write_testdb():
-
-  testtabel = (
-    (1234,'Tom Kooij', '234655232ffd54cb74f6d75aa65132f79c0f404f', 'a8e6833a4588467a0702', 15), # wachtwoord = "geheim"
-    (3141,'Pi','b107f52a6e547eb63666ef74ac39525db2917116', 'salt', 1),
-    (2789,'EEeee','wachtwoord', 'zout', 3),
-    (007,'James Bond','test', 'peper', 7),
-    (1,'Test Leerling','geheim', 'kruiden',1)
-    )
-
-  db = sqlite3.connect(DATABASE)
-
-  with db:
-
-      cur = db.cursor()
-#      cur.execute("INSERT INTO users VALUES (3145,'Pi','wachtwoord',1);")
-#      cur.execute("INSERT INTO users VALUES (2789,'EEeee','wachtwoord',3);")
-#      cur.execute("INSERT INTO users VALUES (007,'James Bond','wachtwoord',7);")
-#      cur.execute("INSERT INTO users VALUES (1,'Test Leerling','geheim',1);")
-
-      cur.execute("DROP TABLE IF EXISTS users")
-      cur.execute("CREATE TABLE users(id INT, naam TEXT, wachtwoord TEXT, salt TEXT, keuze INT )")
-      cur.executemany("INSERT INTO users VALUES(? ?, ?, ?, ?)", testtabel)
-      db.commit()
 
 
 def fix_lastname(s):
@@ -197,7 +152,7 @@ def read_users_and_write_passwords():
 
                 reader.next() # skip first row (legend)
                 for row in reader:
-                    #print row
+                    # print row
                     leerlingnummer = int(row[0])
                     klas = int(row[5][0])
                     email = row[6]
@@ -218,6 +173,7 @@ def read_users_and_write_passwords():
                     write_passwd.writerow([leerlingnummer, naam, hashedpassword, salt, klas, 0])
                     mailmerge.writerow([leerlingnummer,voornaam, password, email, naam])
 
+
 def create_userdb():
     """
     Lees passwd.csv en maak user table
@@ -237,10 +193,9 @@ def create_userdb():
             print row
             usertable.append(row)
 
-    db = MySQLdb.connect(host=MYSQLSERVER, user=MYSQLUSER, db=MYSQLDB, passwd=MYSQLPASS)
+    db = get_db()
 
     with db:
-
         cur = db.cursor()
 
         cur.execute("DROP TABLE IF EXISTS users")
@@ -248,75 +203,6 @@ def create_userdb():
         cur.executemany("INSERT INTO users VALUES(%s, %s, %s, %s, %s, %s)", usertable)
         db.commit()
 
-def sel():
-
-  db = MySQLdb.connect(host='tomkooij.mysql.pythonanywhere-services.com', user='tomkooij', db='tomkooij$aanmeldr', passwd=MYSQLPASS)
-
-  with db:
-      cur = db.cursor()
-
-      cur.execute("select naam,wachtwoord,salt,keuze,klas from users where id = 2651")
-      db.commit()
-
-      row = cur.fetchone()
-      print row
-      print row[0], row[1]
-
-
-def change_ww(leerlingnummer, wachtwoord):
-  db = sqlite3.connect(DATABASE)
-
-  with db:
-    db.row_factory = sqlite3.Row   # this enables the dictionary cursor
-    cur = db.cursor()
-
-    cur.execute("SELECT * FROM users WHERE Id=:Id", {"Id": leerlingnummer})
-    db.commit()
-
-    row = cur.fetchone()
-    db_salt = row["salt"]
-
-    m = hashlib.sha1()
-    m.update(db_salt+wachtwoord)
-    mijn_hash = m.hexdigest()
-
-    cur = db.cursor()
-
-    cur.execute("UPDATE users SET wachtwoord=? WHERE id=?", (mijn_hash,leerlingnummer))
-    db.commit()
-
-
-
-def check_login(username, wachtwoord):
-
-  leerlingnummer = int(username)
-
-  db = sqlite3.connect(DATABASE)
-
-  with db:
-
-      db.row_factory = sqlite3.Row   # this enables the dictionary cursor
-      cur = db.cursor()
-
-      cur.execute("SELECT * FROM users WHERE Id=:Id", {"Id": leerlingnummer})
-      db.commit()
-
-      row = cur.fetchone()
-      db_wachtwoord = row["wachtwoord"]
-      db_salt = row["salt"]
-
-      m = hashlib.sha1()
-      m.update(db_salt+wachtwoord)
-      mijn_hash = m.hexdigest()
-
-      print "naam in database: ", row["naam"]
-      print "wachtwoord in db: ", db_wachtwoord
-      print wachtwoord, mijn_hash
-
-      if (mijn_hash==row["wachtwoord"]):
-        return True
-
-  return False
 
 #
 # random.choice() is size(2^50=1e15)
@@ -331,8 +217,6 @@ def generate_password(length):
     return ''.join([random.choice(pw_set) for c in pw])
 
 
-
-
 def process_workshop_keuzes():
 
     #
@@ -340,7 +224,7 @@ def process_workshop_keuzes():
     #
     ingeschreven = [] # de grote inshrijf lijst
 
-    db = MySQLdb.connect(host=MYSQLSERVER, user=MYSQLUSER, db=MYSQLDB, passwd=MYSQLPASS)
+    db = get_db()
 
     with db:
         cur = db.cursor()
@@ -368,8 +252,9 @@ def process_workshop_keuzes():
     print_workshop_keuzes(ingeschreven)
     #write_ingeschreven(ingeschreven)
 
-# schrijf naar csv
+
 def write_ingeschreven(ingeschreven):
+    """schrijf ingescherven lln + keuzes naar CSV"""
     print "writing: ingeschreven.csv"
     with open('ingeschreven.csv', 'wb') as f:
         mailmerge = csv.writer(f,  dialect='excel')
@@ -377,11 +262,9 @@ def write_ingeschreven(ingeschreven):
             mailmerge.writerow(row)
 
 
-
-
 def save_db_to_csv():
-
-    db = MySQLdb.connect(host=MYSQLSERVER, user=MYSQLUSER, db=MYSQLDB, passwd=MYSQLPASS)
+    """Maak backup.csv"""
+    db = get_db()
 
     with db:
         cur = db.cursor()
@@ -397,13 +280,11 @@ def save_db_to_csv():
 
 def print_workshop_keuzes(ingeschreven):
 
-    db = MySQLdb.connect(host=MYSQLSERVER, user=MYSQLUSER, db=MYSQLDB, passwd=MYSQLPASS)
+    db = get_db()
 
     plaatsen = {}
 
     with db:
-#      db.row_factory = MySQLdb.Row   # this enables the dictionary cursor
-
         cur = db.cursor()
         cur.execute("SELECT id,plaatsen FROM workshops")
 
@@ -444,10 +325,12 @@ def print_workshop_keuzes(ingeschreven):
     print "Nog %d te gaan" % (len(ingeschreven[0])-3)
     print "Er zijn nog %d plaatsen beschikbaar" % nog_vrij
 
+
 def set_plaatsen(workshop_id, plaatsen):
+    """HACK voor database errors"""
     print "Ik zet workshop %d op %d" % (workshop_id, plaatsen)
 
-    db = MySQLdb.connect(host='tomkooij.mysql.pythonanywhere-services.com', user='tomkooij', db='tomkooij$aanmeldr', passwd=MYSQLPASS)
+    db = get_db()
     with db:
         cur = db.cursor()
         cur.execute("UPDATE workshops set plaatsen = %s where id = %s ", [plaatsen, workshop_id])
@@ -456,21 +339,21 @@ def set_plaatsen(workshop_id, plaatsen):
 def set_desc(workshop_id, omschrijving):
     print "Ik zet workshop %d titel op %s" % (workshop_id, omschrijving)
 
-    db = MySQLdb.connect(host='tomkooij.mysql.pythonanywhere-services.com', user='tomkooij', db='tomkooij$aanmeldr', passwd=MYSQLPASS)
+    db = get_db()
     with db:
         cur = db.cursor()
-        cur.execute("UPDATE workshops set titel = %s where id = %s ", [omschrijving, workshop_id])
+        cur.execute("UPDATE workshops set titel = %s where id = %s ",
+                            [omschrijving, workshop_id])
         db.commit()
 
 
-# hotfix voor workshop toevoegen
-#GEVAARLIJK!!!!
 def hotfix_NIETGEBRUIKEN():
+    """HACK Voor het toegoegen van een (vergeten) workshop"""
     print "NIET GEBRUIKEN!!!"
     print "Let's don't and say we did...!?"
     return 0
 
-    db = MySQLdb.connect(host='tomkooij.mysql.pythonanywhere-services.com', user='tomkooij', db='tomkooij$aanmeldr', passwd=MYSQLPASS)
+    db = get_db()
     # heb je deze ook al in de workshops lijst toegevoegd!?!?!?!?
     temp = ((32,"EXTRA: bordspellen",20,bovenbouw),
     (33,"EXTRA: schaken (alleen bovenbouw)",20,bovenbouw))
@@ -484,14 +367,19 @@ def hotfix_NIETGEBRUIKEN():
         db.commit()
     print "Ik hoop dat je dit niet in een live productieomgeving hebt gebruikt.\n Je test het toch nog wel even?"
 
+
 if __name__=='__main__':
-    print "This is db_tools.py!\n"
+    print "db_tools.py!"
+    print ""
     print "Setup database:"
     print "read_users_and_write_passwords() reads users.csv and writes output.csv"
     print "create_userdb() writes user/pass from output.csv to the database"
     print "write_workshops() writes the workshops to the database"
-    print "\nprint_db() to dump the database"
-    print "\nprocess_workshop_keuzes() geeft output"
+    print ""
+    print "print_db() to dump the database"
+    print ""
+    print "process_workshop_keuzes() geeft output"
     print "print workshops geeft workshops zoals ingevoerd"
-    print "set_plaatsen() GEVAARLIJK kan gebruikt worden om aantal plaatsen per workshop te fixen"
+    print "set_plaatsen() GEVAARLIJK kan gebruikt worden om "
+          "aantal plaatsen per workshop te fixen"
     print "save_db_to_csv() maakt een reservekopie van de DB"
